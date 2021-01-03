e7e50cb35ce4b9d2e5787c3fdbb9231cfc962098
Arman Shahinyan <texeltexel2009@gmail.com>
2021-01-03 02:29:17 +0100

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'aybuben',
+      cwd: '/home/wwwaybubenorg/www.aybuben.org',
+      script: 'npm',
+      args: 'start',
+      env: {},
+    },
+  ],
+};
